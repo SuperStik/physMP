7fe244d68322188c829ebfdf32eab702df2cf19f
SuperStik <daviswood290@gmail.com>
2025-09-18 16:34:34 -0700

    Initialize SDL3

--- src/main.c
+++ src/main.c
@@ -1,5 +1,15 @@
+#include <err.h>
 #include <stdio.h>
 
+#include <SDL3/SDL_init.h>
+
 int main(void) {
+	char initialized = SDL_Init(SDL_INIT_VIDEO);
+	if (!initialized)
+		errx(1, "%s", SDL_GetError());
+
 	fputs("Hello physMP!\n", stderr);
+
+	SDL_Quit();
+	return 0;
 }
