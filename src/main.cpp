a14ff9c43104f67da8653721b8e34cb55af42a94
SuperStik <daviswood290@gmail.com>
2025-10-08 16:58:42 -0700

    Add link test with color

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <cstdio>
 #include <err.h>
 
+#include <Jolt/Jolt.h>
+#include <Jolt/Core/Color.h>
 #include <SDL3/SDL_init.h>
 
 #include "metal/main.h"
@@ -9,6 +11,9 @@ int main(void) {
 	char initialized = SDL_Init(SDL_INIT_VIDEO);
 	if (!initialized)
 		errx(1, "%s", SDL_GetError());
+
+	const JPH::Color darkred = JPH::Color::sDarkRed;
+	printf("darkred: %hhu %hhu %hhu\n", darkred(0), darkred(1), darkred(2));
 
 	fputs("Hello physMP!\n", stderr);
 	MTL_main();
