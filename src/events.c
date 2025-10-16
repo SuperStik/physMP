46ba7e9c8e5d9839fcb01a2dfe3e7fdc69626004
SuperStik <daviswood290@gmail.com>
2025-10-16 11:48:33 -0700

    Ignore repeated keyboard inputs

--- src/events.c
+++ src/events.c
@@ -4,11 +4,14 @@
 #include "shared.h"
 
 void ev_key_down(SDL_Window *window, SDL_KeyboardEvent *key) {
-	warnx("keydown %u", key->scancode);
+	if (key->repeat)
+		return;
+
+	warnx("keydown: %u", key->scancode);
 	warnx("ply: %p", &localplayer);
 }
 
 void ev_key_up(SDL_Window *window, SDL_KeyboardEvent *key) {
-	warnx("keyup %u", key->scancode);
+	warnx("keyup: %u", key->scancode);
 	warnx("ply: %p", &localplayer);
 }
